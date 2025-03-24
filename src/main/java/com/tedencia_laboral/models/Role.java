b5eadd7e2f69988e6c401df76113ee1f6546f82e
YovannyRn <yovannyn30@gmail.con>
2025-03-24 22:48:03 +0100

    authorizatio, login and register

---
+++ src/main/java/com/tedencia_laboral/models/Role.java
@@ -0,0 +1,37 @@
+package com.tedencia_laboral.models;
+
+import com.tedencia_laboral.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleName roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ src/main/java/com/tedencia_laboral/services/UserService.java
@@ -0,0 +1,141 @@
+package com.tedencia_laboral.services;
+
+import com.tedencia_laboral.dtos.auth.CheckTokenRequest;
+import com.tedencia_laboral.dtos.auth.LoginRequest;
+import com.tedencia_laboral.dtos.auth.LoginResponse;
+import com.tedencia_laboral.dtos.auth.RegisterRequest;
+import com.tedencia_laboral.models.Role;
+import com.tedencia_laboral.models.User;
+
+import com.tedencia_laboral.models.UserInfo;
+import com.tedencia_laboral.repositories.UserInfoRepository;
+import com.tedencia_laboral.repositories.RoleRepository;
+import com.tedencia_laboral.repositories.UserRepository;
+import com.tedencia_laboral.security.JwtUtil;
+import jakarta.transaction.Transactional;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final UserInfoRepository userInfoRepository;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public UserService(UserRepository userRepository, RoleRepository roleRepository,
+                       UserInfoRepository userInfoRepository, AuthenticationManager authenticationManager,
+                       PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.userInfoRepository = userInfoRepository;
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getUsername())
+                        .password(user.getPassword())
+                        .authorities(user.getRole().getRoleName().name())
+                        .build()
+                ).orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    public void deleteUserById(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public User createUser(RegisterRequest userFromFront) {
+
+        if (this.userRepository.existsByUsername(userFromFront.getUsername())) {
+            throw new IllegalArgumentException("User already exists");
+        }
+
+        else {
+            Role role = this.roleRepository.findByRoleName(userFromFront.getRoleName()).orElseThrow(
+                    () -> new IllegalArgumentException("Role no permitido")
+            );
+
+            User user = new User();
+            user.setUsername(userFromFront.getUsername());
+            user.setPassword(
+                    this.passwordEncoder.encode(userFromFront.getPassword())
+            );
+            user.setRole(role);
+            user = this.userRepository.save(user);
+
+            UserInfo userInfo = new UserInfo();
+            userInfo.setUser(user);
+            userInfo.setFirstName(userFromFront.getFirstName());
+            userInfo.setLastName(userFromFront.getLastName());
+            userInfo.setAddress(userFromFront.getAddress());
+
+            this.userInfoRepository.save(userInfo);
+            return user;
+        }
+
+    }
+
+
+    public LoginResponse login(LoginRequest credentials) {
+        // Comprobamos si el usuario existe
+        User user = this.userRepository.findByUsername(credentials.getUsername()).orElseThrow(
+                () -> new BadCredentialsException("User not found")
+        );
+
+        // Comprobamos si la contraseña no coincide con la que tenemos en la base de datos
+        if (!this.passwordEncoder.matches(credentials.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        LoginResponse loginData = new LoginResponse();
+        loginData.setUsername(credentials.getUsername());
+        loginData.setRole(user.getRole().getRoleName());
+        loginData.setToken(this.jwtUtil.generateToken(credentials.getUsername()));
+
+        return loginData;
+    }
+
+    public boolean checkToken(CheckTokenRequest checkTokenRequest) {
+        return this.jwtUtil.validateToken(
+                checkTokenRequest.getToken(),
+                checkTokenRequest.getUsername()
+        );
+    }
+
+
+}
+
+
+
+
